7b01d72ec63cbb53adc02de12bfe45e3a11c43ba
timo9939 <timothylau20199@gmail.com>
2023-07-05 14:55:28 +0100

    email confirm page finished

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ submitButton.addEventListener("click", form_submission);
 
 const main=document.getElementById("mainPage")
 const successMessage=document.getElementById("success")
+const emailInput=document.getElementById("input")
+let emailDisplay=document.getElementById("client-email")
 
 function form_submission(event){
     event.preventDefault()
     alert('Submit button pressed');
     main.style.display="none";
+    console.log(`Email is ${emailInput.value}`)
+    emailDisplay.textContent=emailInput.value
 
-    successMessage.style.display="flex"
+    successMessage.style.display="block"
 
  
+}
+
+function back_to_homepage(event){
+event.preventDefault()
+confirm('back to homepage')
 }
