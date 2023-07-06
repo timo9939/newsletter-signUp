3ba30c1763ddbedfbd0a77318d7b1ca79fc0a72f
timo9939 <timothylau20199@gmail.com>
2023-07-06 11:37:18 +0100

    dismiss button work

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const successMessage=document.getElementById("success")
 const emailInput=document.getElementById("input")
 let emailDisplay=document.getElementById("client-email")
 
+const successButton=document.getElementById("success-btn")
+successButton.addEventListener("click",back_to_homepage)
+
 function form_submission(event){
     event.preventDefault()
     alert('Submit button pressed');
@@ -19,7 +22,10 @@ function form_submission(event){
  
 }
 
-function back_to_homepage(event){
-event.preventDefault()
-confirm('back to homepage')
+function back_to_homepage(event2){
+event2.preventDefault()
+
+
+successMessage.style.display="none"
+main.style.display="flex";
 }
